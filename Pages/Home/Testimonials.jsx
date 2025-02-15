2edfc815fdad5a0b7a7cd237c9131874302d3a59
dianamutinda <jaydiana25@gmail.com>
2025-02-15 17:37:23 +0300

    creates the testimonial part

---
+++ Pages/Home/Testimonials.jsx
@@ -0,0 +1,54 @@
+import "./Home.css"
+import profile1 from "../../src/assets/profile1.jpg"
+const Testimonials = () => {
+    return (
+      <section>
+        <div className="test-title">
+          <h1 className="title">Testimonials</h1>
+          <h2 className="subtitle">what our beneficiaries say about us</h2>
+        </div>
+    
+        <div className="test-container">
+          
+          <div className="img-container">
+            <div className="img-test-wrapper">
+             <img src={profile1} alt="" />
+             <p className="rating">stars</p>
+              <div className="wrapper-text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequatur, ad!
+                </p>
+                <h4>ruth mutisya</h4>
+              </div>
+            </div>
+  
+            <div className="img-test-wrapper">
+            <img src={profile1} alt="" />
+            <p className="rating">stars</p>
+              <div className="wrapper-text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequatur, ad!
+                </p>
+                <h4>manase gunga</h4>
+              </div>
+            </div>
+  
+            <div className="img-test-wrapper">
+            <img src={profile1} alt="" />
+            <p className="rating">stars</p>
+              <div className="wrapper-text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Consequatur, ad!
+                </p>
+                <h4>paul peterson</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  export default Testimonials;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header/Header"
 import Signin from "../Pages/Authentication/Signin";
 import Signup from "../Pages/Authentication/Signup";
+import Testimonials from "../Pages/Home/Testimonials";
 const app = () =>{
   return(
     <>
     <Header/>
     <Signin/>
     <Signup/>
+    <Testimonials/>
     </>
   )
 }
