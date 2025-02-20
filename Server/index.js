3b0e7dc072b64c1ad55dca9f7d0d59d6e2045c12
dianamutinda <jaydiana25@gmail.com>
2025-02-20 13:36:10 +0300

    creates the user router

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-
+import userRouter from "./user.router.js";
 const app = express();
 
+app.use("/user", userRouter);
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
