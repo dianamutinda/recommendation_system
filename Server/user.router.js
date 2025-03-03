0d88672ae4833c021b3c0a7bce07127ca882f2b8
dianamutinda <jaydiana25@gmail.com>
2025-03-04 00:09:10 +0300

    developing the login route

--- Server/user.router.js
+++ Server/user.router.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import bcrypt from bcrypt
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { Router } from "express";
 
 const router = Router();
@@ -26,7 +27,7 @@ router.post("/register", async(req, res) => {
     })
     if (userWithEmail)
         return res.status (400).json({sucess:false, message:"Email already exists"});
-    const hashedPassword = await bcrypt.hashsync(password, 10);
+    const hashedPassword = await bcrypt.hashSync(password, 10);
     const newUser = await prisma.users.create({
         data:{
             firstName,
@@ -41,6 +42,37 @@ router.post("/register", async(req, res) => {
         console.log(error);
         res.status(500).json({sucess:false, message: "Server error"});
     }
+})
+
+router.post("/login", async(req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await prisma.users.findFirst({
+        where: {email:email}
+    })
+    //res.json(user)
+    if (user){
+        const matchedPassword = await bcrypt.compareSync(password, user.password)
+        if (matchedPassword === true){
+            const payload = {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, 
+                {expiresIn:"10m"});
+            res.cookie("access_token", token);
+            res.status(200).json({sucess: true, data: payload});
+        }else{
+            res.status(401).json({sucess:false, message:"Wrong user credentials"})
+        }
+    }else{
+        res.status(401).json({sucess:false, message:"Wrong email or password"})
+    }
+  } catch (error) {
+    res.status(500).json({sucess:false, message: "Server error"});
+  }
 })
 
 export default router;
