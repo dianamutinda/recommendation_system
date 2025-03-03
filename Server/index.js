4befa381e39b2f6eecd83e4b12c072182bb4f263
dianamutinda <jaydiana25@gmail.com>
2025-03-03 23:22:15 +0300

    developing the register route

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { json } from "express";
 import userRouter from "./user.router.js";
 const app = express();
 
+app.use(json());
 app.use("/", userRouter);
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
--- Server/user.router.js
+++ Server/user.router.js
@@ -1,11 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import bcrypt from bcrypt
 import { Router } from "express";
 
 const router = Router();
 
-router.post("/register", (req, res) => {
-    const {firstName, lastName, password, email} = req.body;
+router.post("/register", async(req, res) => {
+    try {
+        const {firstName, lastName, password, email} = req.body;
 
-    if(!firstName)
+   if(!firstName)
         return res.status (400).json({sucess:false, message: "First name is required"});
 
     if(!lastName)
@@ -16,6 +20,27 @@ router.post("/register", (req, res) => {
 
     if(!email)
         return res.status (400).json({sucess:false, message: "Email is required"});
+
+    const userWithEmail = await prisma.users.findFirst({
+        where:{email:email}
+    })
+    if (userWithEmail)
+        return res.status (400).json({sucess:false, message:"Email already exists"});
+    const hashedPassword = await bcrypt.hashsync(password, 10);
+    const newUser = await prisma.users.create({
+        data:{
+            firstName,
+            lastName,
+            password: hashedPassword,
+            email
+        }
+    })
+
+    return res.status (200).json({sucess:true,  data: newUser});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({sucess:false, message: "Server error"});
+    }
 })
 
 export default router;
